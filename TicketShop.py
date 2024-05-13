2a7928efe571dc4d787cbd277fa9e12a3d9a9600
ellihe <ellinooravi@gmail.com>
2024-05-13 17:15:54 +0200

    Update TicketShop.py
    delete personal information

--- TicketShop.py
+++ TicketShop.py
@@ -1,5 +1,5 @@
 # TIE-02101 Johdatus ohjelmointiin
-# Ellinoora Vikman, 291597, ellinoora.vikman@tuni.fi
+# Ellinoora Hetemaa
 # Bussimaksulaskin, Graafisen käyttöliittymän yksinkertainen projektitehtävä 13.10:
 
 # Bussilippujen ostopalvelu, joka laskee käyttäjän haluaminen
